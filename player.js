9bc4af810883fd4df32c176ac5dbc11a0c357eaa
Luccithekid <126lpetito@frhsd.com>
2026-01-21 10:56:51 -0500

    baseball legends

--- player.js
+++ player.js
@@ -1,10 +1,77 @@
 //Create XMLHttpRequest object
 const xhr = new XMLHttpRequest();
 
-//onlad is an event... like a click event :)
-xhr.onload = function() {
-    if(xhr.status === 200) {
-        const data  = JSON.parse(xhr.responseText);
+xhr.onload = function () {
+
+    if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText);
+        const players = data.players;
+        const container = document.getElementById("cards");
+
+        players.forEach(player => {
+            const card = document.createElement("card");
+            //add a className "card" to the card variable
+
+            let statsHTML = "";
+            let teamsHTML = "";
+            let awardsHTML = "";
+
+            // the for loop below is looping through an object 
+            // the syntax is different when you look through arrays
+            for (let stat in player.careerStats) {
+                statsHTML += "<li>" + stat + ": " + player.careerStats[stat] + "</li>";
+            }
+
+            // teams and awards are both arrays, so these for loops should look the same
+            for (let i = 0; i < player.teams.length; i++) {
+                teamsHTML += "<li>" + player.teams[i] + "</li>";
+            }
+
+           
+            for (let i = 0; i < player.awards.length; i++) {
+                awardsHTML += "<li>" + player.awards[i] + "</li>";
+            }
+
+            card.innerHTML = `
+                <h2>
+                ${player.name}
+                </h2>
+                <p><span class="label">Position:</span>
+               ${player.position}
+                </p>
+                <p><span class="label">Primary Team:</span> 
+                ${player.team}
+                </p>
+                <p><span class="label">Jersey Number:</span>
+                ${player.number}
+                </p>
+                <p><span class="label">Born:</span>
+                ${player.birthDate}
+                </p>
+                <p><span class="label">Throws:</span>
+                ${player.throws}
+                </p>
+                <p><span class="label">Hall of Fame:</span>
+                ${player.halloffame}
+                </p>
+
+                <p class="label">Career Stats:</p>
+                <ul>
+                statsHTML
+                </ul>
+
+                <p class="label">Teams:</p>
+                <ul>
+                teamsHTML
+                </ul>
+
+                <p class="label">Awards:</p>
+                <ul>
+                awardsHTML
+                </ul>`;
+
+            container.appendChild(card); //variables go into those spots
+        )};
     }
 };
 
